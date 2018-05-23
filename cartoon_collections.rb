026fb602b2fc23c930c3adcf8593969d3be29d69
Elaine Anderson <eparie@bu.edu>
2018-05-23 14:27:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.collect.with_index(1) do |name, index|
+    puts "#{index}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  calls_array = []
+  planeteer_calls.collect do |calls|
+  calls.capitalize + "!"
+end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.include? {|i| i > 4}
 end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
