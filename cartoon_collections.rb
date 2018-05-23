4c9ccfabff52c0ba30a5a9f6e8286cbe9a70b306
Elaine Anderson <eparie@bu.edu>
2018-05-23 14:34:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(calls)
 end
 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.include?(cheese_types)
 end
